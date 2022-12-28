72f5e3da6824392f295de12acd4dfb7cdaaa08ce
Grigorov_Vasiliy <Grigorov@internet.ru>
2022-12-28 08:49:56 +0700

    54

---
+++ task 54/Program.cs
@@ -0,0 +1,47 @@
+﻿void ImputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(1, 11);
+    }
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+void UbivanieMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+            {
+                if (matrix[i, k] < matrix[i, k + 1])
+                {
+                    int temp = matrix[i, k + 1];
+                    matrix[i, k + 1] = matrix[i, k];
+                    matrix[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+Console.Clear();
+Console.Write("Введите размер матрицы через пробел, где первое число строки, второе столбцы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+Console.WriteLine("Начальный массив:");
+ImputMatrix(matrix);
+PrintMatrix(matrix);
+Console.WriteLine("Конечный массив:");
+UbivanieMatrix(matrix);
+PrintMatrix(matrix);
